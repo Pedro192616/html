ee5cccfd71b16d0865f59d68c7a7a1063581768a
Pedro192616 <pedroribalc.05@gmail.com>
2025-05-26 19:44:16 +0000

    html

---
+++ funçao.js
@@ -0,0 +1,27 @@
+// Criando uma funçao.
+function somar(a, b) {
+    return a + b
+}
+function subtrair(a, b) {
+    return a - b
+}
+function multiplicar(a, b) {
+    return a . b
+}
+function dividir(a, b) {
+    return a / b
+}
+
+// Chamando a funçao.
+// Adicionando o resultado da funçao constante.
+const soma =  somar(2,3)
+const subtracao =  subtrair(2,3)
+const multiplicacao =  multiplicar(2,3)
+const divisao =  dividir(2,3)
+
+
+// Mostra o conteudo constante.
+console.log('Soma: ${soma}')
+console.log('Subtraçao: ${subtracao}')
+console.log('Multiplicaçao: ${multiplicacao}')
+console.log('Divisao: ${divisao}')
---
+++ operadores.js
@@ -0,0 +1,20 @@
+let nome1 = 'Marta'
+let nome2 = 'Marta'
+
+let idade1 = 10
+let idade2 = '10'
+
+console.log('Igual')
+console.log(nome1 === nome2 )
+console.log(idade1 == idade2)
+console.log(idade1 === idade2)
+
+console.log('\ndiferente')
+console.log(nome1 != nome2)
+console.log(idade1 != idade2)
+
+console.log('\nAND')
+console.log(1 < 2  && 1 < 3)
+console.log(10 < 2  && 1 < 3)
+
+console.log
